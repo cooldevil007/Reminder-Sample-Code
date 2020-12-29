2adc797f54cf3b9fbb73672d51987196ae6aa881
Anand <anand.asingh07@gmail.com>
2020-12-29 21:07:06 +0530

    Create messages.js

---
+++ lambda/logic/messages.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Fajr: 'Time to pray Fajr.',
+    PreSunrise: '25 minutes left to pray Fajr.',
+    Dhuhr: 'Time to pray Dohar.',
+    PreAsr: '25 minutes left to pray Dohar.',
+    Asr: 'Time to pray Asar.',
+    PreMaghrib: '25 minutes left to pray Asar.',
+    Maghrib: 'Time to pray Maghrib.',
+    PreIsha: '25 minutes left to pray Maghrib.',
+    Isha: 'Time to pray Eesha.'
+};
